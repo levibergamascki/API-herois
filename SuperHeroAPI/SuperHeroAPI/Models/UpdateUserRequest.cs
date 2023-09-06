8025205ccf01f3bfcd6a5409c69a84843e010bb4
levibergamascki <lbergamascki@gmail.com>
2023-09-06 11:17:16 -0300

    api pronta

---
+++ SuperHeroAPI/SuperHeroAPI/Models/UpdateUserRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SuperHeroAPI.Models
+{
+    public class UpdateUserRequest
+    {
+        public string name { get; set; }
+        public string password { get; set; }
+        [ForeignKey("IdentityId")]
+        public Guid FavHeroi { get; set; }
+    }
+}
